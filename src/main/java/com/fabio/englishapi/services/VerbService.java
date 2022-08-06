4e3a5d878503a2df0b8f628028c6b54cfaaf5182
Fabio Leandro <fabiolpsolucoes@gmail.com>
2022-08-06 12:07:49 -0300

    Cors e Transactional

--- src/main/java/com/fabio/englishapi/services/VerbService.java
+++ src/main/java/com/fabio/englishapi/services/VerbService.java
@@ -6,6 +6,7 @@ import com.fabio.englishapi.exceptions.ObjectNotFoundException;
 import com.fabio.englishapi.repositories.VerbRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,24 +17,29 @@ public class VerbService {
     @Autowired
     private VerbRepository verbRepository;
 
+    @Transactional
     public List<VerbDto> findAll(){
         return  verbRepository.findAll().stream().map(this::toDto).collect(Collectors.toList());
     }
 
+    @Transactional
     public VerbDto save(VerbDto verbDto){
         return toDto(verbRepository.save(toVerb(verbDto)));
     }
 
+    @Transactional
     public VerbDto findById(Long id){
         return toDto(verbRepository.findById(id)
                 .orElseThrow(()-> new ObjectNotFoundException("Verb not found with id -> "+id)));
     }
 
+    @Transactional
     public VerbDto updateById(Long id, VerbDto verbDto){
         findById(id);
         return toDto(verbRepository.save(toVerb(verbDto)));
     }
 
+    @Transactional
     public void deleteById(Long id){
         findById(id);
         verbRepository.deleteById(id);
