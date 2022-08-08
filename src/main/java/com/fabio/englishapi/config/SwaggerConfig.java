5d74c0c5495eb7d03b1a7e5f6096cef7a7021c16
Fabio Leandro <fabiolpsolucoes@gmail.com>
2022-08-08 10:25:54 -0300

    Implementacao do swagger

---
+++ src/main/java/com/fabio/englishapi/config/SwaggerConfig.java
@@ -0,0 +1,49 @@
+package com.fabio.englishapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    //http://localhost:8080/swagger-ui/
+
+    @Bean
+    public Docket getDocket(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.fabio.englishapi"))
+                .build()
+                .apiInfo(metaData());
+
+    }
+
+    private ApiInfo metaData(){
+        return new ApiInfoBuilder()
+                .title("Spring Boot - English REST API")
+                .description("API to study english")
+                .version("1.0.0")
+                .build();
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/com/fabio/englishapi/controller/VerbController.java
+++ src/main/java/com/fabio/englishapi/controller/VerbController.java
@@ -2,6 +2,8 @@ package com.fabio.englishapi.controller;
 
 import com.fabio.englishapi.dtos.VerbDto;
 import com.fabio.englishapi.services.VerbService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,32 +14,38 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/${api.version}/verbs")
+@Api(tags = "Verb Controller")
 public class VerbController {
 
     @Autowired
     private VerbService verbService;
 
     @GetMapping
+    @ApiOperation(value = "Find all verbs")
     public ResponseEntity<List<VerbDto>> findAll(){
         return ResponseEntity.ok(verbService.findAll());
     }
 
     @PostMapping
+    @ApiOperation(value = "Create a verb")
     public ResponseEntity<VerbDto> saveVerb(@Valid @RequestBody VerbDto verbDto){
         return ResponseEntity.status(HttpStatus.CREATED).body(verbService.save(verbDto));
     }
 
     @GetMapping("/{id}")
+    @ApiOperation(value = "Find a verb by id")
     public ResponseEntity<VerbDto> findById(@PathVariable Long id){
         return ResponseEntity.ok(verbService.findById(id));
     }
 
     @PutMapping("/{id}")
+    @ApiOperation(value = "Update a verb by id")
     public ResponseEntity<VerbDto> updateById(@PathVariable Long id, @Valid @RequestBody VerbDto verbDto){
         return ResponseEntity.ok(verbService.updateById(id,verbDto));
     }
 
     @DeleteMapping("/{id}")
+    @ApiOperation(value = "Delete a verb by id")
     public ResponseEntity<Void> deleteById(@PathVariable Long id){
         verbService.deleteById(id);
         return ResponseEntity.noContent().build();
